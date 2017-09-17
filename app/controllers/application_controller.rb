6e2610dcfc8ae6b337c7b6c7dd394f646fc0086b
sukey <sdklos@gmail.com>
2017-09-17 17:18:31 -0400

    fixing application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
-
+  end
+  
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || root_path
   end
