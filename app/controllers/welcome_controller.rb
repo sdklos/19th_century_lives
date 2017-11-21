eaa034638062d14079d0087b5dc3f2d846430f8c
sukey <sdklos@gmail.com>
2017-11-20 23:15:26 -0500

    jquery index load is working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,9 @@ class WelcomeController < ApplicationController
 
   def patriarchs_and_matriarchs
     @people = Person.patriarchs_and_matriarchs
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @people }
+    end
   end
 end
