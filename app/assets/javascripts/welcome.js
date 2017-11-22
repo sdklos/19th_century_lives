e48488a51fb0ef680e35192156611adc97e8fa47
sukey <sdklos@gmail.com>
2017-11-22 11:45:04 -0500

    changed listener to onclick

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,18 +1,5 @@
-$(document).ready(function() {
-  attachListeners();
-})
-
-function attachListeners() {
-  peopleIndexListener()
-  statesIndexListener()
-  citiesIndexListener()
-}
-
-function closeListener() {
-  $("a.js-close").on("click", function() {
-    var name = $(this).data("name");
-    $("#" + name).html("")
-  })
+function closeIndex(div) {
+    $("#" + div).html("")
 }
 
 function moreListener() {
@@ -23,48 +10,32 @@ function moreListener() {
     $.get("/" + path + "/" + id + ".json", function(item) {
       $("#more-" + id ).html("hi?")
     })
-
   })
 }
 
-function peopleIndexListener() {
-  $(".js-people-index").on("click", function(e) {
-    var path = $(this).data("path");
-    $.get("/" + path + ".json", function(items) {
-      $("#" + path).append(`<h4><a href="#" class="js-close" data-name="${path}">Close</a></h4>`)
-      items.forEach(function(item) {
-        $("#" + path).append(`<p><a href="#" class="js-more" data-id="${item["id"]}" data-path="${path}">` + item["given_name"] + " " + item["name"] + `</a></p><div id="more-${item["id"]}"></div>`)
-      })
-      closeListener()
-      moreListener()
+function loadPeopleIndex(path) {
+  $.get("/" + path + ".json", function(items) {
+    $("#" + path).append(`<h4><a href="#" onclick="closeIndex(id)" id="${path}">Close</a></h4>`)
+    items.forEach(function(item) {
+      $("#" + path).append(`<p><a href="#" class="js-more" data-id="${item["id"]}" data-path="${path}">` + item["given_name"] + " " + item["name"] + `</a></p><div id="more-${item["id"]}"></div>`)
     })
   })
 }
 
-function statesIndexListener() {
-  $("#js-states-index").on("click", function() {
-    var path = $(this).data("path");
-    $.get("/" + path + ".json", function(items) {
-      $("#" + path).append(`<h4><a href="#" class="js-close" data-name="${path}">Close</a></h4>`)
-      items.forEach(function(item) {
-        $("#" + path).append(`<p><a href="#" class="js-more" data-id="item["id"]" data-path="${path}">` + item["name"] + `</a></p><div id="more-${item["id"]}"></div>`)
-      })
-      closeListener()
-      moreListener()
+function loadStatesIndex() {
+  $.get("/states.json", function(items) {
+    $("#states").append(`<h4><a href="#" onclick="closeIndex('states')">Close</a></h4>`)
+    items.forEach(function(item) {
+      $("#states").append(`<p><a href="#" class="js-more" data-id="item["id"]" data-path="states">` + item["name"] + `</a></p><div id="more-${item["id"]}"></div>`)
     })
   })
 }
 
-function citiesIndexListener() {
-  $("#js-cities-index").on("click", function() {
-    var path = $(this).data("path");
-    $.get("/" + path + ".json", function(items) {
-      $("#" + path).append(`<h4><a href="#" class="js-close" data-name="${path}">Close</a></h4>`)
-      items.forEach(function(item) {
-        $("#" + path).append(`<p><a href="#" class="js-more" data-id="${item["id"]}" data-path="${path}">` + item["name"] + "," + " " + item["state"]["abbreviation"] + `</a></p><div id="more-${item["id"]}"></div>`)
-      })
-      closeListener()
-      moreListener()
+function loadCitiesIndex() {
+  $.get("/cities.json", function(items) {
+      $("#cities").append(`<h4><a href="#" onclick="closeIndex('cities')">Close</a></h4>`)
+    items.forEach(function(item) {
+      $("#cities").append(`<p><a href="#" class="js-more" data-id="${item["id"]}" data-path="cities">` + item["name"] + "," + " " + item["state"]["abbreviation"] + `</a></p><div id="more-${item["id"]}"></div>`)
     })
   })
 }
