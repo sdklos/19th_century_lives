19c3caa6f90c6a29afb62df4dc0ed521a2ffc4e0
sukey <sdklos@gmail.com>
2017-11-22 14:15:48 -0500

    index functions complete save for styling and refactoring

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -15,8 +15,24 @@ function loadStatesIndex() {
   $.get("/states.json", function(items) {
     $("#states").append(`<h4><a href="#" onclick="closeIndex('states')">Close</a></h4>`)
     items.forEach(function(item) {
-      $("#states").append(`<p><a href="/states/${item["id"]}">` + item["name"] + `</a></p><div id="more-${item["id"]}"></div>`)
+      $("#states").append(`<p><a href="#" onclick="loadStateInfo(this)" data-id="${item["id"]}">` + item["name"] + `</a></p><div id="more-states-${item["id"]}"></div><div id="more-states-${item["id"]}-cities"></div>`)
     })
+  })
+}
+
+function loadStateInfo(data) {
+  var id = data["dataset"]["id"]
+  var url = "/states/" + id
+  $.get(url + ".json", function(item) {
+    $("#more-states-" + id ).append(`<a href=${url}>View on Separate Page</a><div>`)
+    $("#more-states-" + id + "-cities").append("Cities in the database:<br>")
+    if(item["cities"].length > 0) {
+      item["cities"].forEach(function (city){
+        $("#more-states-" + id + "-cities").append(`<a href="/cities/${city["id"]}">${city["name"]}</a>`)
+      })
+    } else {
+      $("#more-states-" + id + "-cities").append("there are no cities in the database yet")
+    }
   })
 }
 
