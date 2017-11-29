17a53dfc1c41f6874054b6ee1fdf76f2950e88eb
sukey <sdklos@gmail.com>
2017-11-29 18:09:05 -0500

    add new city works now

--- app/assets/javascripts/states.js
+++ app/assets/javascripts/states.js
@@ -5,6 +5,12 @@ function City(attributes) {
   this.state_id = attributes.state.id
 }
 
+City.success = function(json){
+  var city = new City(json);
+  var cityLi = city.renderLI()
+  $("div#new_city").append(cityLi)
+}
+
 $(function(){
   City.templateSource = $("#entry-template").html()
   City.template = Handlebars.compile(City.templateSource)
@@ -14,24 +20,21 @@ City.prototype.renderLI = function(){
   return City.template(this)
 }
 
-$(function(){
-  $("form#new_city").on("submit", function(e) {
-    e.preventDefault()
-    var $form = $(this);
-    var action = $form.attr("action")
-    var params = $form.serialize()
+City.formSubmit = function(e) {
+  e.preventDefault()
+  var $form = $(this);
+  var action = $form.attr("action")
+  var params = $form.serialize()
 
-    $.ajax({
-      url: action,
-      method: "POST",
-      data: params,
-      dataType: "json"
-    })
-    .success(function(json){
-      var city = new City(json);
-      debugger
-      var cityLi = city.renderLI()
-      $("div#new_city").append(cityLi)
-    })
+  $.ajax({
+    url: action,
+    method: "POST",
+    data: params,
+    dataType: "json"
   })
+  .success(City.success)
+}
+
+$(function(){
+  $("form#new_city").on("submit", City.formSubmit)
 })
