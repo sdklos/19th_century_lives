0eda18d93db0049db01dcee43b5baa3d4a30a10c
sukey <sdklos@gmail.com>
2017-11-29 20:25:24 -0500

    state show page and add city form in order

--- app/assets/javascripts/cities.js
+++ app/assets/javascripts/cities.js
@@ -1,1 +1,28 @@
+function City(attributes) {
+  this.name = attributes.name;
+  this.id = attributes.id;
+  this.state_name = attributes.state.name
+  this.state_id = attributes.state.id
+  this.state_abbreviation = attributes.state.abbreviation
+}
 
+City.success = function(json){
+  var city = new City(json);
+  $("div#new_city").prepend(HandlebarsTemplates['states/show'](city))
+  $("#name_input").val("")
+}
+
+City.formSubmit = function(e) {
+  e.preventDefault()
+  var $form = $(this);
+  var action = $form.attr("action")
+  var params = $form.serialize()
+
+  $.ajax({
+    url: action,
+    method: "POST",
+    data: params,
+    dataType: "json"
+  })
+  .success(City.success)
+}
--- app/assets/javascripts/states.js
+++ app/assets/javascripts/states.js
@@ -1,41 +1,7 @@
-function City(attributes) {
-  this.name = attributes.name;
-  this.id = attributes.id;
-  this.state_name = attributes.state.name
-  this.state_id = attributes.state.id
-  this.state_abbreviation = attributes.state.abbreviation
-}
-
-City.success = function(json){
-  var city = new City(json);
-  var cityLi = city.renderLI()
-  $("div#new_city").append(cityLi)
-}
-
-$(".states.show").ready(function() {
-  City.templateSource = $("#entry-template").html()
-  City.template = Handlebars.compile(City.templateSource)
-})
-
-City.prototype.renderLI = function(){
-  return City.template(this)
-}
-
-City.formSubmit = function(e) {
-  e.preventDefault()
-  var $form = $(this);
-  var action = $form.attr("action")
-  var params = $form.serialize()
-
-  $.ajax({
-    url: action,
-    method: "POST",
-    data: params,
-    dataType: "json"
-  })
-  .success(City.success)
-}
-
 $(function(){
   $("form#new_city").on("submit", City.formSubmit)
+})
+
+$(".states.show").ready(function() {
+
 })
