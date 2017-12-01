5c7257528633213a72a7133b18c478b0c0853457
sukey <sdklos@gmail.com>
2017-11-30 20:50:52 -0500

    added prototype

--- app/assets/javascripts/states.js
+++ app/assets/javascripts/states.js
@@ -1,11 +1,16 @@
 function State(attributes) {
   this.name = attributes.name;
   this.id = attributes.id;
-  this.cities = attributes.cities.sort(function(a, b) {
+  this.cities = attributes.cities
+}
+
+State.prototype.alphabetizeCities = function() {
+  return this.cities.sort(function(a, b) {
     if(a.name < b.name) return -1;
     if(a.name > b.name) return 1;
     return 0;
   })
+
 }
 
 $(function(){
